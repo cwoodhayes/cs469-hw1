c1f6ce7f7e49415ea24bf90257885b40238c23d7
conorbot <cwoodhayes@gmail.com>
2025-10-15 17:26:10 -0500

    constructing the map from the dataset works

--- hw0/map.py
+++ hw0/map.py
@@ -2,6 +2,9 @@
 Map of the world according to the robot, represented in world coordinates
 """
 
+from __future__ import annotations
+from dataclasses import dataclass
+
 import numpy as np
 
 from hw0.data import Dataset
@@ -12,41 +15,57 @@ class Map:
     Map of the world in a grid form
     """
 
-    def __init__(self, map_dimensions: np.ndarray, cell_size: float, obstacles: list[np.ndarray], start: np.ndarray, goal: np.ndarray) -> None:
+    @dataclass(frozen=True)
+    class Config:
         """
-        :param map_dimensions: dimensions of the map in meters [[xmin, xmax], [ymin, ymax]]
-        :type map_dimensions: np.ndarray[[xlim],[ylim]]
+        :param dimensions: dimensions of the map in meters [[xmin, xmax], [ymin, ymax]]
+        :type dimensions: np.ndarray[[xlim],[ylim]]
         :param cell_size: side size of an individual square cell in meters
         :type cell_size: tuple[float, float]
-        :param obstacles: Nx2 array of obstacles, where each row is an (x,y) location of an obstacle in world coords
         :param start: start location in world coords (x,y)
         :param goal: goal location in world coords (x,y)
         """
-        dim = np.array(map_dimensions)
+
+        dimensions: np.ndarray
+        cell_size: float
+        start: np.ndarray
+        goal: np.ndarray
+
+    def __init__(self, config: Config, obstacles: list[np.ndarray]) -> None:
+        """
+        :param config: configuration for the map
+        :param obstacles: Nx2 array of obstacles, where each row is an (x,y) location of an obstacle in world coords
+        """
+        self._c = config
+        dim = np.array(config.dimensions)
         self._dim = dim
-        self._world_mins = dim[:,0]
-        self._cell_size = cell_size
-        x_range = dim[0,1] - dim[0,0]
-        y_range = dim[1,1] - dim[1,0]
+        self._world_mins = dim[:, 0]
+        self._cell_size = config.cell_size
+        x_range = dim[0, 1] - dim[0, 0]
+        y_range = dim[1, 1] - dim[1, 0]
         range = np.array([y_range, x_range])
 
-        shape = range / cell_size
+        shape = range / config.cell_size
         shape_int = shape.round().astype(int)
         if not np.isclose(shape_int, shape).all():
-            raise ValueError(f"{dim} grid does not break evenly into cells of size {cell_size}")
+            raise ValueError(
+                f"{dim} grid does not break evenly into cells of size {config.cell_size}"
+            )
 
         self.grid = np.zeros(shape=shape_int, dtype=int)
+        # maintain this list mostly for debugging
+        self._obstacles = []
 
         self.add_obstacles(obstacles)
-        start_loc = self.world_coords_to_grid_index(start)
-        goal_loc = self.world_coords_to_grid_index(goal)
+        start_loc = self.world_coords_to_grid_index(config.start)
+        goal_loc = self.world_coords_to_grid_index(config.goal)
         self.grid[start_loc] = 2
         self.grid[goal_loc] = 3
-    
+
     def world_coords_to_grid_index(self, coord: np.ndarray) -> tuple[int, int]:
         """
         Convert world coordinates (as [x,y]) into grid indices (row,col) of the cell containing that location
-        
+
         :param coord: world coordinate in [x,y]
         :type coord: np.ndarray
         :return: grid indices [row, col]
@@ -56,23 +75,29 @@ class Map:
         rounds = (coord / self._cell_size - self._world_mins).round().astype(int)
         loc = (rounds[1], rounds[0])
         return loc
-    
+
     def add_obstacles(self, obstacles: list[np.ndarray]) -> None:
         """
         Add a set of obstacles to the map.
         (we only ever add them, we never remove)
-        
+
         :param obstacles: Nx2 list of obstacles, where each row is an (x,y) location of an obstacle in world coords
         """
         for obs in obstacles:
             row, col = self.world_coords_to_grid_index(obs)
             self.grid[row, col] = 1
-    
+            self._obstacles.append(obs)
+
     @classmethod
-    def construct_from_dataset(cls, ds: Dataset) -> None:
+    def construct_from_dataset(cls, ds: Dataset, config: Config) -> Map:
         """
         construct a map with all the obstacles from the dataset
-        
+
         :param ds: Dataset object
         :type ds: Dataset
         """
+        obstacles = [
+            np.array((x, y)) for x, y in zip(ds.landmarks["x_m"], ds.landmarks["y_m"])
+        ]
+
+        return cls(config, obstacles)
--- hw0/test_map.py
+++ hw0/test_map.py
@@ -1,40 +1,66 @@
 """
 tests for map.py
 """
+
+from pathlib import Path
 import numpy as np
 import pytest
 
+from hw0.data import Dataset
 from hw0.map import Map
+
+REPO_ROOT = Path(__file__).parent.parent
 
 
 @pytest.fixture(scope="function")
 def default_empty_map() -> Map:
-    DEFAULT_DIMS=np.array([
-        [-2, 5],
-        [-6, 6],
-    ])
+    DEFAULT_DIMS = np.array(
+        [
+            [-2, 5],
+            [-6, 6],
+        ]
+    )
     DEFAULT_CELL_SIZE = 1.0
 
-    return Map(
+    cfg = Map.Config(
         DEFAULT_DIMS,
         DEFAULT_CELL_SIZE,
-        [],
         np.array([0.5, -1.5]),
         np.array([0.5, 1.5]),
     )
 
+    return Map(cfg, [])
 
-@pytest.mark.parametrize("world_coord, expected_idx",
-                         [
-                            (
-                                (0, 0), (6, 2)
-                            ),
-                            (
-                                (-1.5, -5.5), (0, 0)
-                            )
-                         ])
+
+@pytest.mark.parametrize(
+    "world_coord, expected_idx", [((0, 0), (6, 2)), ((-1.5, -5.5), (0, 0))]
+)
 def test_world_coord_to_idx(world_coord, expected_idx, default_empty_map):
     coord = np.array(world_coord)
     loc = default_empty_map.world_coords_to_grid_index(coord)
     assert loc[0] == expected_idx[0]
     assert loc[1] == expected_idx[1]
+
+
+def test_map_from_ds():
+    p = REPO_ROOT / "data/ds1"
+    ds = Dataset.from_dataset_directory(p)
+
+    DEFAULT_DIMS = np.array(
+        [
+            [-2, 5],
+            [-6, 6],
+        ]
+    )
+    DEFAULT_CELL_SIZE = 1.0
+
+    cfg = Map.Config(
+        DEFAULT_DIMS,
+        DEFAULT_CELL_SIZE,
+        np.array([0.5, -1.5]),
+        np.array([0.5, 1.5]),
+    )
+
+    map = Map.construct_from_dataset(ds, cfg)
+
+    assert np.sum(map.grid == 1) == len(ds.landmarks)
